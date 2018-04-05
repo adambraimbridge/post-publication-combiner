af66a7510a4a4b5fbe7ecf0bf5f6941cfc4c966f
andrei.ciobanu <andrei.ciobanu@ft.com>
2018-04-05 14:03:54 +0300

    fix/dredd tests

--- _ft/dredd-hooks.js
+++ _ft/dredd-hooks.js
@@ -2,38 +2,38 @@ var hooks = require('hooks');
 var http = require('http');
 var fs = require('fs');
 
-const defaultFixtures = './_ft/ersatz-fixtures.yml';
+// const defaultFixtures = './_ft/ersatz-fixtures.yml';
 
-function sleepFor( sleepDuration ){
-    var now = new Date().getTime();
-    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
-}
+// function sleepFor( sleepDuration ){
+//     var now = new Date().getTime();
+//     while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+// }
 
-hooks.beforeAll(function(t, done) {
-   if(!fs.existsSync(defaultFixtures)){
-      console.log('No fixtures found, skipping hook.');
-      done();
-      return;
-   }
+// hooks.beforeAll(function(t, done) {
+//    if(!fs.existsSync(defaultFixtures)){
+//       console.log('No fixtures found, skipping hook.');
+//       done();
+//       return;
+//    }
 
-   var contents = fs.readFileSync(defaultFixtures, 'utf8');
+//    var contents = fs.readFileSync(defaultFixtures, 'utf8');
 
-   var options = {
-      host: 'localhost',
-      port: '9000',
-      path: '/__configure',
-      method: 'POST',
-      headers: {
-         'Content-Type': 'application/x-yaml'
-      }
-   };
+//    var options = {
+//       host: 'localhost',
+//       port: '9000',
+//       path: '/__configure',
+//       method: 'POST',
+//       headers: {
+//          'Content-Type': 'application/x-yaml'
+//       }
+//    };
 
-   var req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-   });
+//    var req = http.request(options, function(res) {
+//       res.setEncoding('utf8');
+//    });
 
-   req.write(contents);
-   req.end();
-   sleepFor(5000) // sleep in order to allow ersatz to load the sent config
-   done();
-});
+//    req.write(contents);
+//    req.end();
+//    sleepFor(5000) // sleep in order to allow ersatz to load the sent config
+//    done();
+// });
