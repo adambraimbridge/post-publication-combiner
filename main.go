d8de050ae506a62c949220dc8ff60603398e6292
marina <marina.chompalova@ft.com>
2020-01-09 10:21:38 +0200

    Make channel buffered

--- main.go
+++ main.go
@@ -266,7 +266,7 @@ func routeRequests(port *string, requestHandler *requestHandler, healthService *
 }
 
 func waitForSignal() {
-	ch := make(chan os.Signal)
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	<-ch
 }
